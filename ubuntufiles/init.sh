736b3b7d7b1fe8748e1bfb860f3a4db67af6d442
Thomas Haller <thomashaller@gmx.at>
2024-09-02 10:20:47 +0200

    init.sh file does now the setup of an RPC password

--- ubuntufiles/init.sh
+++ ubuntufiles/init.sh
@@ -4,5 +4,7 @@
 # creating a random password for the rpcuser and rpcpassword
 # only if file /root/.DMDV3/diamond.conf is empty
 if [ ! -s /root/.DMDV3/diamond.conf ]; then
+  # this command will create the .DMDV3 directory and the diamond.conf file
+  diamondd | true  
   echo -e "rpcuser=diamondrpc\nrpcpassword=$(openssl rand -hex 32)" > /root/.DMDV3/diamond.conf
 fi
